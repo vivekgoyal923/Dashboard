18964bfcaa2327929ae1e5ef9a67fbd9f9de6128
vivek <vivekgoyal923@gmail.com>
2021-02-25 16:30:22 +0800

    Initial Dashboard Commits

---
+++ main.py
@@ -0,0 +1,66 @@
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import base64
+import os
+from dash.dependencies import Input, Output
+import datetime
+
+
+app = dash.Dash(__name__)
+
+def generate_thumbnail(image,count):
+    style = {'padding': '5px', 'fontSize': '16px', 'text-align': 'left'}
+    return html.Div([
+        html.Img(
+            src='data:image/jpg;base64,{}'.format(image.decode()),
+            style={
+                'height': 70,
+                'width': 50,
+                'position': 'relative',
+                'padding-top': 10,
+                'padding-right': 10
+            }
+        ),
+        html.Span("Detected: {}".format(count), style=style)
+    ])
+
+
+app.layout = html.Div(
+    html.Div([
+        html.H1(children="No Mask: Wall of Shame"),
+        html.Div(id="date-update"),
+        html.H3(children="Faces Detected Today"),
+        html.Div(id="image-update"),
+        dcc.Interval(
+            id='interval-component',
+            interval=5 * 1000,  # in milliseconds
+            n_intervals=0
+        )
+    ])
+)
+
+
+@app.callback(Output('image-update', 'children'),
+              [Input('interval-component', 'n_intervals')])
+def update_image(n):
+    images_div = []
+    image_path = "C:/Users/vivek/PycharmProjects/FaceMaskDetection/DetectedFaces/"
+    listOfPics = os.listdir(image_path)
+    for i in listOfPics:
+        count = int(i.split(" ")[2][:-4])
+        encoded_image = base64.b64encode(open(image_path + i, 'rb').read())
+        images_div.append(generate_thumbnail(encoded_image, count))
+    return images_div
+
+
+@app.callback(Output('date-update', 'children'),
+              [Input('interval-component', 'n_intervals')])
+def update_image(n):
+    now = datetime.datetime.now()
+    return [html.H2(now.strftime("%Y-%m-%d %H:%M:%S"))]
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
